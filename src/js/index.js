7ebbfa224df0afc20a68596ca5cd5ab54f9bbfd4
Ryan Will Darós <ryan_will-@hotmail.com>
2023-03-15 22:57:16 -0300

    :sparkles: Adicionado o JS para criar o gráfico com o Axes Labels

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,103 @@
-var options = {
-  chart: {
-    type: "area",
-    stacked: true,
-  },
-  colors: ["#7C3AED"],
-  stroke: {
-    width: 3,
-  },
+const options = {
   series: [
     {
-      name: "sales",
-      data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+      name: "cambio",
+      data: [
+        {
+          x: new Date("2018-02-12").getTime(),
+          y: 5.18,
+        },
+        {
+          x: new Date("2018-02-13").getTime(),
+          y: 5.3,
+        },
+        {
+          x: new Date("2018-02-14").getTime(),
+          y: 5.18,
+        },
+        {
+          x: new Date("2018-02-15").getTime(),
+          y: 5.11,
+        },
+        {
+          x: new Date("2018-02-16").getTime(),
+          y: 5.18,
+        },
+        {
+          x: new Date("2018-02-17").getTime(),
+          y: 5.25,
+        },
+        {
+          x: new Date("2018-02-18").getTime(),
+          y: 5.18,
+        },
+        {
+          x: new Date("2018-02-19").getTime(),
+          y: 5.2,
+        },
+      ],
     },
   ],
+  chart: {
+    height: 350,
+    type: "area",
+    toolbar: {
+      show: false,
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "straight",
+  },
+  yaxis: {
+    min: 5,
+    tickAmount: 4,
+    // labels: {
+    //   formatter: (value) => {
+    //     return value.toFixed(1).replace(".", ",");
+    //   },
+    // },
+  },
   xaxis: {
-    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
     labels: {
-      datetimeFormatter: {
-        year: "yyyy",
-        month: "MMM 'yy",
-        day: "dd MMM",
-        hour: "HH:mm",
-      },
+      show: false,
+    },
+    tooltip: {
+      enabled: false,
+    },
+    axisTicks: {
+      show: false,
     },
   },
   fill: {
-    type: "gradient",
-    colors: "#7C3AED",
+    gradient: {
+      shadeIntensity: 1,
+      opacityFrom: 0.7,
+      opacityTo: 0.9,
+      stops: [0, 90, 100],
+    },
+  },
+  colors: ["#7C3AED"],
+  tooltip: {
+    custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+      return `<div class="tooltip">
+    <span>${String(series[seriesIndex][dataPointIndex]).replace(
+      ".",
+      ","
+    )}</span>
+    <span>${new Date(
+      w.globals.seriesX[seriesIndex][dataPointIndex]
+    ).toLocaleDateString("pt-BR", {
+      weekday: "short",
+      month: "short",
+      day: "numeric",
+    })}</span>
+    </div>`;
+    },
   },
 };
 
-var chart = new ApexCharts(document.querySelector("#chart"), options);
-
+const chart = new ApexCharts(document.querySelector("#chart"), options);
 chart.render();
